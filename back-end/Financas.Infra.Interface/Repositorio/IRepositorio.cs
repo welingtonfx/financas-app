7a0086aae161a8d828c8e350ea8eba6701b5f85b
Welington Wilton <welingtonfx@gmail.com>
2020-03-29 18:26:19 -0300

    Correção interface repositório base

--- back-end/Financas.Infra.Interface/Repositorio/IRepositorio.cs
+++ back-end/Financas.Infra.Interface/Repositorio/IRepositorio.cs
@@ -1,5 +1,8 @@
 ﻿using Financas.Dominio.Interface;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Financas.Infra.Interface.Repositorio
@@ -8,9 +11,13 @@ namespace Financas.Infra.Interface.Repositorio
         where T : IIdentificador
     {
         Task<IEnumerable<T>> Obter();
+        Task<IEnumerable<T>> Obter(Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, params Expression<Func<T, object>>[] propriedadesInclude);
         Task<T> ObterPorId(int id);
+        Task Inserir(IEnumerable<T> entidades);
         Task<T> Inserir(T entidade);
+        Task Alterar(IEnumerable<T> entidades);
         Task<T> Alterar(T entidade);
+        Task Excluir(IEnumerable<int> ids);
         Task Excluir(int id);
     }
 }
